0473b08fa7f4891e8341f76e56ea62f221e6d677
Chen Yi Jun <onearmy1997@gmail.com>
2019-10-22 23:10:02 +0800

    Add new ReadOnly<ModelComponents> classes

---
+++ src/main/java/seedu/address/model/ReadOnlyItinerary.java
@@ -0,0 +1,28 @@
+package seedu.address.model;
+
+import java.time.LocalDate;
+import javafx.collections.ObservableList;
+import seedu.address.model.field.Name;
+import seedu.address.model.day.Day;
+
+/**
+ * Unmodifiable view of an Itinerary
+ */
+public interface ReadOnlyItinerary {
+
+    /**
+     * Returns an unmodifiable view of the itinerary.
+     * This list will not contain any duplicate days.
+     */
+    ObservableList<Day> getItinerary();
+
+    /**
+     * Returns the name of the planner.
+     */
+    Name getName();
+
+    /**
+     * Returns the start date of the planner.
+     */
+    LocalDate getStartDate();
+}
